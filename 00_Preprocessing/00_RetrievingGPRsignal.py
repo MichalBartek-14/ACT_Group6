a0183be4c5e9c378e20942c0d5f52d5050539bc2
MichalBartek-14 <michalbartek147@gmail.com>
2025-06-21 13:56:18 +0200

    Restructure the code of 00_RetrievingGPRsignal.py into comprehensible function

--- 00_Preprocessing/00_RetrievingGPRsignal.py
+++ 00_Preprocessing/00_RetrievingGPRsignal.py
@@ -1,46 +1,60 @@
 import geopandas as gpd
-import shapely
 import json
 import subprocess
 import os
 
-# --- Input Files ---
-input_laz = r"C:\Users\misko\Documents\Michal\Master\RS Integration\ACT_6\Data\WUR_ACT_PG_250515\WUR_ACT_PG_250515\LAZ_Euroradar\TvGPark.laz"
-gpkg = r"C:\Users\misko\Documents\Michal\Master\RS Integration\ACT_6\Valid_loc_3.gpkg"
+def process_lidar_data(input_laz, gpkg, output_las, target_crs="EPSG:28992"):
+    """
+    Processes LiDAR data by cropping it to a specified polygon and saving the output.
 
-output_las = "Proefsleuf_3.las"  # output file name
-target_crs = "EPSG:28992"  # Set to your desired CRS (e.g., UTM zone 33N)
+    :param input_laz: Path to the input LAZ file.
+    :param gpkg: Path to the GeoPackage file containing the polygon for cropping.
+    :param output_las: Path to the output LAS file.
+    :param target_crs: Coordinate Reference System to use for the output.
+    """
 
-# --- Load Shapefile ---
-gdf = gpd.read_file(gpkg)
-gdf = gdf.to_crs(target_crs)
+    # Load Shapefile
+    gdf = gpd.read_file(gpkg)
+    gdf = gdf.to_crs(target_crs)
 
-# --- Construct PDAL Pipeline ---
-pipeline = {
-    "pipeline": [
-        {
-            "type": "readers.las",
-            "filename": input_laz,
-            "spatialreference": target_crs  # Set CRS since original has none
-        },
-        {
-            "type": "filters.crop",
-            "polygon": gdf.geometry.union_all().wkt
-        },
-        {
-            "type": "writers.las",
-            "filename": output_las,
-            "compression": "none"
-        }
-    ]
-}
+    # PDAL Pipeline to read the point cloud of the location
+    pipeline = {
+        "pipeline": [
+            {
+                "type": "readers.las",
+                "filename": input_laz,
+                "spatialreference": target_crs
+            },
+            {
+                "type": "filters.crop",
+                "polygon": gdf.geometry.union_all().wkt
+            },
+            {
+                "type": "writers.las",
+                "filename": output_las,
+                "compression": "none"
+            }
+        ]
+    }
 
-# --- Write and Run PDAL Pipeline ---
-pipeline_path = "pipeline.json"
-with open(pipeline_path, "w") as f:
-    json.dump(pipeline, f, indent=4)
+    # Write and Run PDAL Pipeline
+    pipeline_path = "pipeline.json"
+    with open(pipeline_path, "w") as f:
+        json.dump(pipeline, f, indent=4)
 
-subprocess.run(["pdal", "pipeline", pipeline_path])
+    subprocess.run(["pdal", "pipeline", pipeline_path])
 
-# --- Clean up ---
-os.remove(pipeline_path)
+    # Clean up
+    os.remove(pipeline_path)
+
+#usage:
+"""
+Note: user must have the GPR point cloud of the larger area in the LAZ file format alongside
+ the gpkg file of the desired location they intend to intersect.
+ The function produces a new .las file with coordinate system assigned.
+"""
+process_lidar_data(
+     input_laz=r"C:\Users\misko\Documents\Michal\Master\RS Integration\ACT_6\Data\WUR_ACT_PG_250515\WUR_ACT_PG_250515\LAZ_Euroradar\TvGPark.laz",
+     gpkg=r"C:\Users\misko\Documents\Michal\Master\RS Integration\ACT_6\Valid_loc_3.gpkg",
+     output_las="Proefsleuf_test.las"
+ )
